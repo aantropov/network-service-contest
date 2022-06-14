64474a36227c67ab9c92b4b206196309dea92a55
perevalovds <perevalovds@gmail.com>
2022-06-14 16:56:09 +0300

    tests

---
+++ Benchmark/04_RunFull.bat
@@ -0,0 +1,1 @@
+for /l %%x in (1, 1, 36) do start cmd /C Call WebClient.exe 700 1398 24 172.22.137.65
---
+++ Benchmark/05_RunHuge.bat
@@ -0,0 +1,1 @@
+for /l %%x in (1, 1, 1) do start cmd /C Call WebClient.exe 3000 1 2 172.22.137.65
